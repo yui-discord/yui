79612611266163b470f03cbad30292f6ecd1ea0d
miguelrisquelme <miguelrisquelme@gmail.com>
2021-06-01 08:30:41 -0300

    Added .env file on project

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+require("dotenv").config();
 
 client.on("ready", () => {
 	console.log(
@@ -16,4 +17,4 @@ client.on("message", (msg) => {
 	}
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN_DISCORD);
