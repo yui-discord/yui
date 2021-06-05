14be398d8b928f97d6ede3031bfee25dbe32b395
miguelrisquelme <miguelrisquelme@gmail.com>
2021-06-04 23:25:48 -0300

    adding command meme and nodemon config

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## References
 
+- <a href="https://discordjs.guide/">Discord.js Documentation</a>
 - <a href="https://discordjs.guide/command-handling/#individual-command-files">How to organize your commands</a>
 - <a href="https://discordjs.guide/popular-topics/canvas.html#basic-image-loading">Basic image loading</a>
+- <a href="https://youtu.be/2qOXf5u6SiM">Meme command</a>
+- <a href="https://youtu.be/2qOXf5u6SiM">API Memes</a>
---
+++ commands/meme.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+	name: "meme",
+	description: "Um meme aleatório",
+	async execute(message, args) {
+		const response = await axios({
+			method: "GET",
+			url: "https://meme-api.herokuapp.com/gimme",
+		});
+		const memeEmbed = new Discord.MessageEmbed()
+			.setTitle(response.data.title)
+			.setImage(response.data.url)
+			.setFooter(`${response.data.subreddit} ${response.data.postLink}`);
+
+		let msg = await message.channel.send("Fetching your meme..");
+		msg.edit(memeEmbed);
+	},
+};
