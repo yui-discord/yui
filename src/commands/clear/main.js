1d7cc2d999b7a8a34d71ad7a5f4399170f6cdb26
miguelrisquelme <miguelrisquelme@gmail.com>
2021-06-14 20:59:15 -0300

    command clear and details on poll

---
+++ src/commands/clear/main.js
@@ -0,0 +1,20 @@
+function execute(message) {
+  if (message.author.username === "miguelrisquelme") {
+    message.channel
+      .bulkDelete(100, true)
+      .then((messages) =>
+        message.channel.send(
+          `***Foram deletadas ${messages.size} mensagens papa***`
+        )
+      )
+      .catch(console.error);
+  } else {
+    message.channel.send(`*Acha mesmo que vai limpar o chat plebeu?* 😂`);
+  }
+}
+
+module.exports = {
+  name: "clear",
+  description: "Limpar mensagens, 100 ou o máximo que conseguir",
+  execute,
+};
--- src/commands/poll/functions/poll.js
+++ src/commands/poll/functions/poll.js
@@ -1,12 +1,16 @@
 const Discord = require("discord.js");
 
 const poll = (message, args) => {
+  const channel = message.client.channels.cache.find(
+    (channel) => channel.id == "791686556581756969"
+  );
+
   const messageEmbed = new Discord.MessageEmbed()
     .setTitle("Votação")
     .setColor("#fdd092")
     .setDescription(args.join(" "))
     .setTimestamp();
-  message.channel.send({ embed: messageEmbed }).then((embedMessage) => {
+  channel.send({ embed: messageEmbed }).then((embedMessage) => {
     embedMessage.react("⬆");
     embedMessage.react("⬇");
   });
