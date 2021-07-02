4ceb84a27c7674d52c74269d1534e213692042b9
miguelrisquelme <miguelrisquelme@gmail.com>
2021-07-01 21:07:12 -0300

    waifu command

--- src/commands/anime/main.js
+++ src/commands/anime/main.js
@@ -1,18 +1,23 @@
 const search = require("./functions/search");
 const list = require("./functions/list");
 const img = require("./functions/img");
+const sfw = require("./functions/sfw");
 
 function execute(message, args) {
-  if (args[0] == "search") {
+  if (args[0] == "find" || args[0] == "search") {
     args.shift();
     search(message, args);
-  }
-  if (args == "list") {
-    list(message);
   }
   if (args[0] == "img") {
     args.shift();
     img(message, args);
+  }
+  if (args == "list") {
+    list(message);
+  }
+  if (args[0] == "sfw") {
+    args.shift();
+    sfw(message, args);
   }
 }
 
